960874e926b0c2d193e2da5cb263549c1c201784
msgreentea <msgreentea8@gmail.com>
2021-11-23 17:28:34 +0900

    via mac

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
   target.classList.toggle('open');
+  const nav = document.getElementById("nav-hamburger");
+  nav.classList.toggle('in');
 });
